b1ad5198dbb34a97c60d41b2c08544b6c9883913
Bajuware93 <timo.engler@web.de>
2018-02-05 23:52:29 +0100

    lifting state up

---
+++ components/lifting/parent.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Parent extends React.Component{
+  constructor(props) {
+    super(props);
+      this.state = {
+        value: ''
+      };
+      this.handleClick = this.handleClick.bind(this);
+    }
+
+  handleClick(event){
+    this.setState({value: event.target.value});
+    event.preventDefault();
+  }
+
+  render(){
+    return(
+      <InputComponent value={this.state.value} onClick={this.handleClick} />
+    );
+  }
+}
+
+class InputComponent extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({
+      value: event.target.value
+    });
+    this.props.onClick(event);
+  }
+
+  render(){
+    return(
+      <input type="text" value={this.state.value} onChange={this.handleChange}></input>
+    );
+  }
+}
+
+export default Parent;
